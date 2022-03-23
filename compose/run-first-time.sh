10c9a249af11803f0eb3c3180860344f0bf9e42d
comcon1 <comcon1@protonmail.com>
2022-03-23 11:01:08 +0300

    fix default container name in first-run and add 30s gap

--- compose/run-first-time.sh
+++ compose/run-first-time.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker-compose up -d
-cat my.sql | docker exec -i mariadb_1 mysql -utppuser -pYourSecretPassword tppforcefield
+echo "Waiting for 30 seconds to allow MySQL server start..."
+sleep 30 
+cat my.sql | docker exec -i compose_mariadb_1 mysql -utppuser -pYourSecretPassword tppforcefield
