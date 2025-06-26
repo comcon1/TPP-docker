6a53ad1d652e46c3719e80a4b7f64f1999fa26a9
Alexey Nesterenko <comcon1@protonmail.com>
2025-06-26 23:46:20 +0200

    Fix user permissions. Prioritize 'docker compose' instead of 'docker-compose'.

--- compose/cleanup.sh
+++ compose/cleanup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-docker-compose -p tpproject down
+source .env
+
+if docker compose version &> /dev/null; then
+  DOCKER_COMPOSE="docker compose"
+elif command -v docker-compose &> /dev/null; then
+  DOCKER_COMPOSE="docker-compose"
+else
+  echo "Error: Neither docker-compose nor docker compose command is available."
+  exit 1
+fi
+
+${DOCKER_COMPOSE} -p ${PREFIX} down
 rm -fr volume
